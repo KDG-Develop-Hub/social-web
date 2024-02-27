3507ba48becc55ce7dc7e5577e23b018d6e7cbdb
imeankenshin <omurakenshin@gmail.com>
2024-02-27 18:40:32 +0900

    PandaCSSのセットアップ

---
+++ components/ui/Button.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { css } from "styled-system/css";
+</script>
+
+<template>
+  <button
+    :class="
+      css({
+        border: 'none',
+        borderRadius: '99rem',
+        fontSize: '0.875rem',
+        fontWeight: 500,
+        height: '2.5rem',
+        padding: '0 1rem',
+        bgColor: 'blue.800',
+        color: 'white',
+      })
+    "
+  >
+    <slot>Click me</slot>
+  </button>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,29 @@
+import { createResolver } from "@nuxt/kit";
+const { resolve } = createResolver(import.meta.url);
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  components: [
+    {
+      path: "~/components",
+      pathPrefix: false,
+    },
+  ],
   app: {
     head: {
       title: "kdg.social",
-    }
-  }
-})
+    },
+  },
+  alias: {
+    "styled-system": resolve("./styled-system"),
+  },
+
+  css: ["@/assets/css/global.css"],
+
+  postcss: {
+    plugins: {
+      "@pandacss/dev/postcss": {},
+    },
+  },
+});
