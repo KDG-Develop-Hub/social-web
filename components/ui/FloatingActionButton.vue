abe1401dfb4e8e15d2c6f4b02540eb43b9f593aa
imeankenshin <omurakenshin@gmail.com>
2024-03-16 14:56:24 +0900

    FABの追加

---
+++ components/ui/FloatingActionButton.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <button class="container square">
+    <slot />
+  </button>
+</template>
+
+<style scoped>
+.container {
+  display: grid;
+  place-items: center;
+  border-radius: 0.75rem;
+  border: none;
+  cursor: pointer;
+  background-color: var(--blue-7);
+  --square-size: 3.5rem;
+}
+</style>
