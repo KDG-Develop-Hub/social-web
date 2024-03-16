e8591dd698949a635ad2a034929be0db9d998bb8
imeankenshin <omurakenshin@gmail.com>
2024-03-16 13:02:48 +0900

    変更の訂正

---
+++ nuxt.config.ts
@@ -0,0 +1,31 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    app: {
+        head: {
+            title: "kdg.social",
+        },
+    },
+    components: [
+        {
+            path: "~/components",
+            pathPrefix: false,
+        },
+    ],
+    css: ["@/assets/css/global.css"],
+    devtools: { enabled: true },
+    googleFonts: {
+        families: {
+            "Zen Maru Gothic": [400, 500, 600, 700],
+            "JetBrains Mono": [400],
+        },
+    },
+    imports: {
+        presets: [
+            {
+                from: "clsx",
+                imports: ["clsx"],
+            },
+        ],
+    },
+    modules: ["@nuxtjs/google-fonts"],
+});
