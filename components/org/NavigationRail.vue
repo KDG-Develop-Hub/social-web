cc424f1acb90526d8ab30f8f33d0a2470a348888
imeankenshin <omurakenshin@gmail.com>
2024-03-17 09:42:41 +0900

    ナビゲーションレールをコンポーネント化

---
+++ components/org/NavigationRail.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+import {Compass, Feather, History, Settings2, User, Users} from "lucide-vue-next";
+
+const linkContents = ref([
+  {to: "/", icon: History, text: "アプデ"},
+  {to: "/browse", icon: Compass, text: "見つける"},
+  {to: "/u/user", icon: User, text: "君とは"},
+  {to: "/acquaintances", icon: Users, text: "知り合い"},
+  {to: "/setting", icon: Settings2, text: "せってー"}
+])
+</script>
+
+<template>
+  <nav class="navigation-rail h-stack">
+    <FloatingActionButton>
+      <Feather color="white"/>
+    </FloatingActionButton>
+    <div class="h-stack link-list">
+      <NuxtLink v-for="linkContent in linkContents" :to="linkContent.to" class="h-stack link">
+        <span class="indicator v-stack">
+          <component :is="linkContent.icon" />
+        </span>
+        {{ linkContent.text }}
+      </NuxtLink>
+    </div>
+  </nav>
+</template>
+
+<style scoped>
+.navigation-rail {
+  height: 100%;
+  box-sizing: border-box;
+  width: 5.5rem;
+  padding: 2rem 0;
+  --stack-gap: 2.5rem;
+}
+
+.link-list {
+  --stack-gap: 0.75rem;
+}
+
+.link {
+  font-size: 0.75rem;
+  font-weight: 500;
+  color: black;
+  text-decoration: none;
+  --stack-gap: 0.25rem;
+}
+
+.indicator {
+  width: 3.5rem;
+  justify-content: center;
+  border-radius: var(--radius-round);
+  height: 2rem;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,46 +1,10 @@
 <script setup lang="ts">
-import {Feather, History, Compass, User, Users, Settings2} from "lucide-vue-next";
+import NavigationRail from "~/components/org/NavigationRail.vue";
 </script>
 
 <template>
   <div class="layout">
-    <nav class="navigation-rail h-stack">
-      <FloatingActionButton>
-        <Feather color="white"/>
-      </FloatingActionButton>
-      <div class="h-stack block-list">
-        <NuxtLink to="/" class="h-stack block">
-          <span class="indicator v-stack">
-            <History/>
-          </span>
-          アプデ
-        </NuxtLink>
-        <NuxtLink to="/browse" class="h-stack block">
-          <span class="indicator v-stack">
-            <Compass/>
-          </span>
-          見つける
-        </NuxtLink>
-        <NuxtLink :to="`/u/user`" class="h-stack block">
-          <span class="indicator v-stack">
-            <User />
-          </span>
-          君とは
-        </NuxtLink>
-        <NuxtLink to="/acquaintances" class="h-stack block">
-          <span class="indicator v-stack">
-            <Users />
-          </span>
-          知り合い
-        </NuxtLink>
-        <NuxtLink to="/setting" class="h-stack block">
-          <span class="indicator v-stack">
-            <Settings2 />
-          </span>
-          せってー
-        </NuxtLink>
-      </div>
-    </nav>
+    <NavigationRail/>
     <main class="main">
       <slot/>
     </main>
@@ -57,30 +21,4 @@ import {Feather, History, Compass, User, Users, Settings2} from "lucide-vue-next
   width: 100%;
 }
 
-.navigation-rail {
-  height: 100%;
-  box-sizing: border-box;
-  width: 5.5rem;
-  padding: 2rem 0;
-  --stack-gap: 2.5rem;
-}
-
-.block-list {
-  --stack-gap: 0.75rem;
-}
-
-.block {
-  font-size: 0.75rem;
-  font-weight: 500;
-  color: black;
-  text-decoration: none;
-  --stack-gap: 0.25rem;
-}
-
-.indicator {
-  width: 3.5rem;
-  justify-content: center;
-  border-radius: var(--radius-round);
-  height: 2rem;
-}
 </style>
