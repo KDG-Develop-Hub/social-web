fb065c383abd5e94c91347007b3218ac68723488
imeankenshin <omurakenshin@gmail.com>
2024-05-18 15:51:19 +0900

    名前の変更

--- pages/settings/index.vue
+++ pages/settings/index.vue
@@ -3,7 +3,9 @@
 
 <template>
   <h2>アカウント情報</h2>
-  <Input id="hi" full-width hint="英数半角"/>
+  <Input full-width label="ユーザID"/>
+  <Input full-width maxLength="12" label="ユーザID" hint="英数半角"/>
+  <Input full-width label="メールアドレス" suffix="@gmail.com" hint="英数半角"/>
 </template>
 
 <style scoped>
