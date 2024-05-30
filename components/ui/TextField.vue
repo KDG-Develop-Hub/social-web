9577705d9aceebeb145defb943a8e67d1dd53816
imeankenshin <omurakenshin@gmail.com>
2024-05-30 15:24:51 +0900

    複数行の追加

--- components/ui/TextField.vue
+++ components/ui/TextField.vue
@@ -15,6 +15,7 @@ const props = defineProps<{
 const randomId = props.id ?? useId()
 const hintId = props.hint ? `hint-${randomId}` : undefined
 const model = defineModel<string>()
+const textareaRows = computed(() => Math.max(model.value ? model.value.split("\n").length : 1, 2))
 const prefixRef = ref<HTMLSpanElement>()
 const labelPadding = 4
 const containerPadding = 16
@@ -29,9 +30,14 @@ const clickHandler = () => {
     <div @click="clickHandler" class="container h-stack">
       <span v-if="prefix" class="prefix" ref="prefixRef">{{ prefix }}</span>
       <div class="h-stack input-wrapper full-width full-height">
-        <label class="h-stack label" :for="randomId">{{ label }}</label>
-        <textarea v-if="multiLine" class="full-width full-height input" :maxlength="maxLength" :minLength="minLength" :autocomplete="autocomplete" :id="randomId" :aria-describedby="hintId" :type="type" v-model="model"/>
-        <input v-else class="full-width full-height input" :maxlength="maxLength" :minLength="minLength" :autocomplete="autocomplete" :id="randomId" :aria-describedby="hintId" :type="type" v-model="model"/>
+        <span class="label-wrapper h-stack">
+          <label class="h-stack label" :for="randomId">{{ label }}</label>
+        </span>
+        <textarea v-if="multiLine" class="full-width full-height input" :rows="textareaRows" :maxlength="maxLength"
+                  :minLength="minLength" :autocomplete="autocomplete" :id="randomId" :aria-describedby="hintId"
+                  v-model="model"/>
+        <input v-else class="full-width full-height input" :maxlength="maxLength" :minLength="minLength"
+               :autocomplete="autocomplete" :id="randomId" :aria-describedby="hintId" :type="type" v-model="model"/>
         <span v-if="suffix" class="suffix">{{ suffix }}</span>
       </div>
     </div>
@@ -47,78 +53,103 @@ const clickHandler = () => {
   padding: 0.5rem 2px;
   overflow: hidden;
 }
+
 .container {
   --this-height: 3.5rem;
   gap: 0;
   position: relative;
   outline: var(--color-outline) solid 1px;
-  height: var(--this-height);
+  box-sizing: border-box;
+  min-height: var(--this-height);
   border-radius: 0.25rem;
+  padding: 1rem 0;
   transition: outline-color 200ms, outline-width 50ms;
+
   &:hover {
     outline-color: var(--color-on-surface);
   }
+
   &:focus-within {
     outline-color: var(--color-primary);
     outline-width: 2px;
   }
+
   & > :first-child {
     padding-left: calc(1px * v-bind(containerPadding));
   }
+
   & > :last-child {
     padding-right: calc(1px * v-bind(containerPadding));
   }
 }
+
 .prefix {
   white-space: nowrap;
   user-select: none;
   padding-right: 0.25rem;
 }
+
 .suffix {
   white-space: nowrap;
   user-select: none;
   color: var(--color-on-surface-variant);
 }
+
+.label-wrapper {
+  position: absolute;
+  height: var(--this-height);
+  top: 0;
+}
+
 .label {
   user-select: none;
-  position: absolute;
   white-space: nowrap;
   padding: 0 calc(1px * v-bind(labelPadding));
   background-color: var(--color-surface);
   transition: translate 200ms, font-size 200ms, color 200ms;
   translate: 0;
+
   :is(.wrapper[data-full=true], .wrapper:focus-within) & {
     translate: v-bind(lengthToLeftEnd) calc(var(--this-height) / -2);
     font-size: 0.75rem;
   }
+
   .wrapper:focus-within & {
     color: var(--color-primary);
   }
 }
+
 .input-wrapper {
   gap: 0;
 }
+
 .input {
   padding: 0;
   height: min-content;
   cursor: text;
   font-size: 1rem;
+  min-height: 1.5rem;
+  resize: none;
   border: none;
   outline: none;
   background: transparent;
 }
+
 .supporting-text-wrapper {
   display: flex;
   justify-content: space-between;
   gap: 0.5rem;
   margin: 0.25rem 1rem 0;
 }
+
 .hint, .counter {
   font-size: 0.75rem;
   color: var(--color-on-surface-variant);
 }
+
 .counter {
   text-align: right;
+
   .hint + & {
     width: min-content;
   }
