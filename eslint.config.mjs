cc6d1896675f40beac492ba9778a8dc9f247d642
imeankenshin <omurakenshin@gmail.com>
2024-06-04 14:04:02 +0900

    eslintとprettierの設定

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,6 +13,6 @@ export default withNuxt({
     'vue/multi-word-component-names': 'off',
     'vue/no-multiple-template-root': 'off',
     'no-console': 'error',
-    'no-debugger': 'error',
+    'no-debugger': 'error'
   }
 })
