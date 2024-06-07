6311de4e4558821d13347376067252f533568ce3
takeru <take.okamura@icloud.com>
2024-06-07 17:45:08 +0900

    ツイートダイアログの実装

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { Dialog as ArkDialog } from '@ark-ui/vue'
 import { Compass, Feather, History, Settings2, User, Users } from 'lucide-vue-next'
 
 const currentUser = useCurrentUserStore()
@@ -15,9 +16,29 @@ const linkContents = ref([
   <div class="layout">
     <MaterialNavigationRail>
       <template #fab>
-        <MaterialFAB>
-          <Feather />
-        </MaterialFAB>
+        <ArkDialog.Root>
+          <ArkDialog.Trigger as-child>
+            <MaterialFAB>
+              <Feather />
+            </MaterialFAB>
+          </ArkDialog.Trigger>
+          <MaterialDialog as-child>
+            <ArkDialog.Title> 今回は何を綴るのかな？</ArkDialog.Title>
+            <ArkDialog.Description as-child>
+              <p class="description">
+                自分の考えや出来事を気楽に書こう！コミュニティーガイドラインの確認も忘れないでねッ！
+              </p>
+              <div class="border-bottom"/>
+            </ArkDialog.Description>
+            <MaterialTextField label="内容" type="text" max-length="256" helper-text="0/256" />
+            <template #buttons>
+              <ArkDialog.CloseTrigger as-child>
+                <MaterialButton variant="text">やっぱやめる</MaterialButton>
+              </ArkDialog.CloseTrigger>
+              <MaterialButton color="primary">広めちゃう</MaterialButton>
+            </template>
+          </MaterialDialog>
+        </ArkDialog.Root>
       </template>
       <template #link-list>
         <MaterialNavigationRailLink
@@ -39,6 +60,15 @@ const linkContents = ref([
 .layout {
   display: flex;
   height: 100vh;
+}
+
+.dialog {
+  width: 35px;
+  overflow: visible;
+}
+
+.border-bottom {
+  border-bottom: solid 1px #fff;
 }
 
 main {
