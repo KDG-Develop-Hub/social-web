935eb1ef65a647e9aaf3dbbaac28cd28e817409b
衣笠みのる <168079463+minoru-kinugasa-105@users.noreply.github.com>
2024-09-18 18:07:36 +0900

    500ページの実装

---
+++ pages/500.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="error-container">
+    <h1 class="error-emoticon">_(x0x_)zz</h1>
+    <h2 class="error-title">問題が起きました。</h2>
+    <p class="error-description">
+      ページの読み込みに問題が発生しちゃった（；；）もう一度ロードしなおしてください。
+    </p>
+  </div>
+</template>
