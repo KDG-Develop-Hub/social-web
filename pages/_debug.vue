a40002e0cde0395cc3d02528e7e4024e0d535b4d
imeankenshin <omurakenshin@gmail.com>
2024-10-16 18:09:06 +0900

    デバッグページを追加

---
+++ pages/_debug.vue
@@ -0,0 +1,13 @@
+<script setup>
+  function emits505() {
+    throw Error('This error occured by purpose.')
+  }
+</script>
+
+<template>
+  <div>
+    <h1>Debug Page</h1>
+    <p>This is a debug page.</p>
+    <MaterialButton @click="emits505">エラーを起こす</MaterialButton>
+  </div>
+</template>
