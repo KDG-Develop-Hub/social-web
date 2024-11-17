6d76ee128a4a35a1c619f700c21fe5d5b3fa58c1
imeankenshin <omurakenshin@gmail.com>
2024-11-17 11:44:23 +0900

    エラーページにて、displayを使用

--- pages/error.vue
+++ pages/error.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="error-container">
-    <h1 class="error-display-lg">_(x0x_)zz</h1>
-    <h2 class="error-display-md">問題が起きました。</h2>
-    <p class="error-body-md">
+    <h1 class="display-lg">_(x0x_)zz</h1>
+    <h2 class="display-md">問題が起きました。</h2>
+    <p class="body-md">
       ページの読み込みに問題が発生しちゃった（；；）もう一度ロードしなおしてください。
     </p>
   </div>
