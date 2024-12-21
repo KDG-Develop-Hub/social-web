2f60c8791f1960ae15ab7398468a9bf813b74dc6
imeankenshin <omurakenshin@gmail.com>
2024-12-21 21:08:36 +0900

    ロゴを追加

--- layouts/auth.vue
+++ layouts/auth.vue
@@ -1,18 +1,29 @@
 <template>
   <div class="card-positioner">
     <div class="card">
+      <img class="logo" src="/logo.svg" alt="logo" />
       <slot />
     </div>
   </div>
 </template>
 
 <style scoped>
+  .logo {
+    height: 48px;
+    width: fit-content;
+    margin-bottom: 1rem;
+  }
   .card-positioner {
     display: grid;
     place-items: center;
     height: 100vh;
   }
   .card {
+    width: 100%;
+    max-width: 640px;
+    height: 384px;
+    display: flex;
+    flex-direction: column;
     background-color: var(--md-sys-color-surface-container);
     border-radius: var(--md-sys-shape-corner-lg);
     padding: 1.5rem;
