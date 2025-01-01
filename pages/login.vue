c7b47ab53e0ed1e070d1e6d61b29517c910ae02c
imeankenshin <omurakenshin@gmail.com>
2025-01-01 21:53:32 +0900

    認証ページのレイアウトと、最初の入力項目にフォーカスする挙動を追加

--- pages/login.vue
+++ pages/login.vue
@@ -17,24 +17,26 @@
     undefined,
     { immediate: false }
   )
+  const reset = () => {
+    email.value = ''
+    mailIsSent.value = false
+  }
 </script>
 
 <template>
-  <form
-    v-if="!mailIsSent"
-    class="column full-height"
-    @submit.prevent="execute()"
-  >
-    <div class="login-form-fields">
+  <form v-if="!mailIsSent" class="login-form" @submit.prevent="execute()">
+    <div class="login-form-content">
       <h2 class="headline-md">ログイン</h2>
       <p class="body-md">
         入力されたメールアドレス宛に、ログインするためのリンクを送信します。
       </p>
       <MaterialTextField
         v-model="email"
+        v-focus
         required
         label="メールアドレス"
         type="email"
+        autocomplete="email"
       />
     </div>
     <div class="button-set">
@@ -43,18 +45,34 @@
       </MaterialButton>
     </div>
   </form>
-  <div v-else>
-    <h2 class="headline-md">ログイン用のメールを送信しました</h2>
-    <p class="body-md">
-      ご自身のメールアドレスにログイン用のメールを送信しました。
-      <br />
-      メールに記載されたリンクをクリックして、ログインしてください。
-    </p>
+  <div v-else class="login-form">
+    <div class="login-form-content">
+      <h2 class="headline-md">ログイン用のメールを送信しました</h2>
+      <p class="body-md">
+        ご自身のメールアドレスにログイン用のメールを送信しました。
+        <br />
+        メールに記載されたリンクをクリックして、ログインしてください。
+      </p>
+    </div>
+    <div class="button-set">
+      <MaterialButton
+        variant="outlined"
+        :disabled="isLoading"
+        @click="reset()"
+      >
+        メールを再入力する
+      </MaterialButton>
+    </div>
   </div>
 </template>
 
 <style lang="css" scoped>
-  .login-form-fields {
+  .login-form {
+    display: flex;
+    flex-direction: column;
+    flex-grow: 1;
+  }
+  .login-form-content {
     display: flex;
     flex-direction: column;
     gap: 1rem;
@@ -62,6 +80,8 @@
   }
   .button-set {
     display: flex;
+    flex-grow: 1;
+    align-items: flex-end;
     justify-content: flex-end;
     gap: 1rem;
   }
--- pages/signup.vue
+++ pages/signup.vue
@@ -13,21 +13,21 @@
 
 <template>
   <form v-if="tokenIsValid" @submit.prevent>
-    <h2 class="headline-md">新規登録</h2>
-    <p class="body-md">メールアドレスとユーザーIDを入力してください。</p>
-    <div class="full-height">
-      <MaterialTextField label="メールアドレス" type="email" required />
+    <div class="signup-form-content">
+      <h2 class="headline-md">新規登録</h2>
+      <p class="body-md">メールアドレスとユーザーIDを入力してください。</p>
+      <MaterialTextField v-focus label="メールアドレス" type="email" required />
       <MaterialTextField label="ユーザーID" required />
     </div>
     <div class="button-set">
       <MaterialButton type="submit">新規登録</MaterialButton>
     </div>
   </form>
-  <div v-else-if="tokenIsValid === false">
+  <div v-else-if="tokenIsValid === false" class="signup-form-content">
     <h2 class="headline-md">エラー</h2>
     <p class="body-md">トークンが無効です。</p>
   </div>
-  <div v-else class="signup">
+  <div v-else class="signup-form-content">
     <h2 class="headline-md">新規登録するには...</h2>
     <p class="body-md">
       サービス管理者に直接お問い合わせてね。<br />
@@ -37,18 +37,16 @@
 </template>
 
 <style scoped>
-  .signup {
-    width: 100%;
-    gap: 1rem;
-  }
-  .signup-form {
+  .signup-form-content {
     display: flex;
     flex-direction: column;
     gap: 1rem;
+    height: 100%;
   }
   .button-set {
     display: flex;
     gap: 1rem;
     justify-content: flex-end;
+    flex-grow: 1;
   }
 </style>
