5e73e7fd065a7c0bd8847068da3b0a41577aa59c
imeankenshin <omurakenshin@gmail.com>
2025-01-17 18:53:17 +0900

    不足したtypographyの種類の追加と、つぶやきの表示

--- pages/tweet.vue
+++ pages/tweet.vue
@@ -1,24 +1,56 @@
 <script setup lang="ts">
-  const tweets: Post[] =
-    Array.from({ length: 40 }, () => ({
-      id: '1',
-      userId: '1',
-      userName: 'test',
-      userImageUrl: 'https://via.placeholder.com/256',
-      content: 'test',
-      createdAt: new Date().toISOString(),
-      bookmarkedUserIds: [],
-      imageUrls: [],
-      reactions: null,
-      updatedAt: null
-    }))
-
+  const tweet = {
+    id: '1',
+    userId: 'nihonkokukenpou',
+    userName: '日本国憲法',
+    userImageUrl: 'https://via.placeholder.com/256',
+    content: `日本国民は、正当に選挙された国会における代表者を通じて行動
+し、われらとわれらの子孫のために、諸国民との協和による成果
+と、わが国全土にわたつて自由のもたらす恵沢を確保し、政府の行
+為によつて再び戦争の惨禍が起ることのないやうにすることを決意
+し、ここに主権が国民に存することを宣言し、この憲法を確定す
+る。そもそも国政は、国民の厳粛な信託によるものであつて、その
+権威は国民に由来し、その権力は国民の代表者がこれを行使し、そ
+の福利は国民がこれを享受する。これは人類普遍の原理であり、こ
+の憲法は、かかる原理に基くものである。われらは、これに反する
+一切の憲法、法令及び詔勅を排除する。`,
+    createdAt: new Date().toISOString(),
+    bookmarkedUserIds: [],
+    imageUrls: [],
+    reactions: null,
+    updatedAt: null
+  }
+  const replies: Post[] = Array.from({ length: 40 }, () => ({
+    id: '1',
+    userId: '1',
+    userName: 'test',
+    userImageUrl: 'https://via.placeholder.com/256',
+    content: 'test',
+    createdAt: new Date().toISOString(),
+    bookmarkedUserIds: [],
+    imageUrls: [],
+    reactions: null,
+    updatedAt: null
+  }))
 </script>
 
 <template>
   <div class="tweet-page">
+    <div class="tweet">
+      <MaterialAvatar
+        size="md"
+        :name="tweet.userName"
+        :src="tweet.userImageUrl"
+      />
+      <div>
+        <div class="body-lg">{{ tweet.userName }}</div>
+        <div class="label-md tweeter-user-id">@{{ tweet.userId }}</div>
+        <p class="body-lg">{{ tweet.content }}</p>
+      </div>
+    </div>
+    <MaterialDivider />
     <div class="reply-list">
-      <OrgTweet v-for="tweet in tweets" :key="tweet.id" :post="tweet" />
+      <OrgTweet v-for="reply in replies" :key="reply.id" :post="reply" />
     </div>
     <div class="reply-input-positioner">
       <TweetReplyInput />
@@ -27,10 +59,19 @@
 </template>
 
 <style scoped>
+  .tweet {
+    display: flex;
+    gap: 0.5rem;
+  }
   .tweet-page {
     display: flex;
     flex-direction: column;
+    gap: 1rem;
     height: 100%;
+  }
+  .tweeter-user-id {
+    user-select: all;
+    color: var(--md-sys-color-on-surface-variant);
   }
   .reply-list {
     flex-grow: 1;
