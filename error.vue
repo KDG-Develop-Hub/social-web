d085342e83e50d470b2c97f9b3287e652ff82c0a
imeankenshin <omurakenshin@gmail.com>
2025-01-26 17:06:04 +0900

    エラーの表示を改善

--- error.vue
+++ error.vue
@@ -7,16 +7,24 @@
 </script>
 
 <template>
-  <ErrorContent v-if="error.statusCode === 404">
-    <template #title>ページが見つかりませんでした。</template>
-    <template #description>
-      与えられたURLに対応するページは存在しないか、削除されました...
-    </template>
-  </ErrorContent>
-  <ErrorContent v-else>
-    <template #title>問題が起きました。</template>
-    <template #description>
-      ページの読み込みに問題が発生しちゃった（；；）もう一度ロードしなおしてください。
-    </template>
-  </ErrorContent>
+  <main class="main">
+    <ErrorContent v-if="error.statusCode === 404">
+      <template #title>ページが見つかりませんでした。</template>
+      <template #description>
+        与えられたURLに対応するページは存在しないか、削除されました...
+      </template>
+    </ErrorContent>
+    <ErrorContent v-else>
+      <template #title>問題が起きました。</template>
+      <template #description>
+        ページの読み込みに問題が発生しちゃった（；；）もう一度ロードしなおしてください。
+      </template>
+    </ErrorContent>
+  </main>
 </template>
+
+<style scoped>
+  .main {
+    height: 100dvh;
+  }
+</style>
